a3bf7fc08648ae2e10c190bc83f962ba3ce0fe2a
Nookaraju Maddila <nmaddila@rajus-mbp.infor.com>
2017-12-20 15:41:36 +0530

    initial commit

---
+++ iOS/InforAuthentication.framework/Headers/InforAuthentication.h
@@ -0,0 +1,22 @@
+//
+//  InforAuthentication.h
+//  InforAuthentication
+//  Created by Nookaraju Maddila on 12/09/17.
+//  Copyright © 2017 Infor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InforAuthentication.
+FOUNDATION_EXPORT double InforAuthenticationVersionNumber;
+
+//! Project version string for InforAuthentication.
+FOUNDATION_EXPORT const unsigned char InforAuthenticationVersionString[];
+
+
+
+#import <InforAuthentication/AuthenticationManager.h>
+#import <InforAuthentication/SSODataManager.h>
+#import <InforAuthentication/Definitions.h>
+
+
